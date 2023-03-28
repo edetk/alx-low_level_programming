6bc8d0e71742de9077fa9506b8f3dca184fd3b27
edetk <kufrewilson.edet@gmail.com>
2023-03-28 12:29:23 -0700

    100-101

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,20 +1,14 @@
 #include "main.h"
-#include <stdio.h>
-
 /**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    int a;
-    int b;
+ * swap_int - swap variable values
+ * @a: pointer 1
+ * @b: pointer 2
+ * Return: void
+*/
 
-    a = 98;
-    b = 42;
-    printf("a=%d, b=%d\n", a, b);
-    swap_int(&a, &b);
-    printf("a=%d, b=%d\n", a, b);
-    return (0);
+void swap_int(int *a, int *b)
+{
+*a += *b;
+*b = *a - *b;
+*a = *a - *b;
 }
